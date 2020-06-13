3dac95794038ca5dbf644c880e7a5cd5ac22fc1a
LeeCue <787504485@qq.com>
2020-06-13 21:14:18 +0800

    fixed bug

--- src/main/java/com/xust/sims/entity/NoticeBoard.java
+++ src/main/java/com/xust/sims/entity/NoticeBoard.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -15,13 +16,13 @@ import java.util.Date;
 @Data
 public class NoticeBoard {
     private Integer id;
-    @NotNull(message = "标题不能为空")
+    @NotEmpty(message = "标题不能为空")
     private String title;
     @NotNull(message = "发布状态不能为空")
     private Boolean published;
-    @NotNull(message = "内容不能为空")
+    @NotEmpty(message = "内容不能为空")
     private String content;
-    @NotNull(message = "公告类型名称不能为空")
+    @NotEmpty(message = "公告类型名称不能为空")
     private String typeName;
     @JsonFormat(pattern = "yyyy-MM-dd")
     private Date createTime;
